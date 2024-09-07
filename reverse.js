87787be0beac5c3b601d536de41192be90280166
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-07 17:31:21 +0600

    array reversing using for of loop reverse loop

---
+++ reverse.js
@@ -0,0 +1,32 @@
+// we will reverse this array.
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(numbers);
+
+// numbers.reverse();
+// const number = numbers.reverse();
+// console.log(number);
+
+// using for of
+const rev_numbers = [];
+for (let num of numbers) {
+    // console.log(num);
+    rev_numbers.unshift(num);
+}
+// console.log(rev_numbers);
+
+// reverse using for loop
+const reversed_numbers = [];
+for (let i = 0; i < numbers.length; i++) {
+    const num = numbers[i];
+    reversed_numbers.unshift(num);
+}
+// console.log(reversed_numbers);
+
+// reversed loop using for loop
+const revRevNumbers = [];
+for (let i = numbers.length - 1; i >= 0; i--) {
+    const num = numbers[i];
+    console.log(num);
+    revRevNumbers.push(num);
+}
+console.log(revRevNumbers);
