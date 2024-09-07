17337879a2efdc6104d15103db4ab62673545959
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-07 17:08:29 +0600

    loop traversal using for loop

--- looping.js
+++ looping.js
@@ -6,3 +6,20 @@
  * 4. for of --> array loop korar jonno
  * 5. for in --> object loop korar jonno
  */
+
+const friends = ['Elon', 'Bill', 'Mark', 'Waren'];
+
+for (const friend of friends) {
+    // console.log(friend);
+}
+
+for (let i = 0; i < friends.length; i++) {
+    // console.log(i);
+    // console.log(friends[i]);
+}
+
+const numbers = [5, 56, 4847, 87, 879, 998, 95];
+for (let i = 0; i < numbers.length; i++) {
+    console.log(i);
+    console.log(numbers[i]);
+}
